ab3b168dc1186ec625f8be8d7803f631b7d72236
Gavin  peterson <gavinpeterson@guest-10-166-146-36.wireless.umn.edu>
2018-08-02 19:19:47 -0500

    created new structure

--- README.md
+++
@@ -1,4 +0,0 @@
-# LightHouse Mental Health
-
-## Heroku:
-https://protected-atoll-80193.herokuapp.com/
--- lighthouse/src/App.js
+++ lighthouse/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Entry from './components/Journal/Entry'
 
 class App extends Component {
   render() {
     return (
       <Entry />
-    )
+    );
   }
 }
 
